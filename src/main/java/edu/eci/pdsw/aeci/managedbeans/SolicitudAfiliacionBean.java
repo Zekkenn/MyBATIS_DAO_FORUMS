374d5d57649af4383ffc1caf50bb3fe9ec27aec0
JohanRamirezC <johan.ramirez-c@mail.escuelaing.edu.co>
2016-11-03 18:30:31 -0500

    Estructura basica de archivos para el proyecto

---
+++ src/main/java/edu/eci/pdsw/aeci/managedbeans/SolicitudAfiliacionBean.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.aeci.managedbeans;
+
+/***
+importar aca las clases que necesiten
+import edu.eci.pdsw.samples.aeci.;
+
+***/
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
+
+/**
+ *
+ * @author hcadavid
+ */
+@ManagedBean(name = "solicitudCuenta")
+@SessionScoped
+
+public class SolicitudAfiliacionBean implements Serializable{
+    
+}
---
+++ src/main/java/edu/eci/pdsw/aeci/persistence/mybatisimpl/MyBatisDAOUser.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.pdsw.aeci.persistence.mybatisimpl;
+
+import edu.eci.pdsw.aeci.entities.*;
+
+/**
+ *
+ * @author 2095498
+ */
+public class MyBatisDAOUser {
+    
+}
