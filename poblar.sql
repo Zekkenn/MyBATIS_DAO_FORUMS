f786fd7c17a4c36abf22d93f92aa8a6cee75b98c
Hernan Felipe Losada <hernan.losada96@gmail.com>
2016-11-09 16:07:15 -0500

    adelanto

---
+++ poblar.sql
@@ -0,0 +1,33 @@
+/* Poblar Programs*/
+
+insert into Program values (1,'Ingenieria Civil',10)
+
+insert into Program values (2,'Ingenieria Electronica',10)
+
+insert into Program values (3,'Ingenieria Electrica',10)
+
+insert into Program values (4,'Ingenieria De Sistemas',10)
+
+insert into Program values (5,'Ingenieria Industrial',10)
+
+insert into Program values (6,'Matematicas',10)
+
+insert into Program values (7,'Economia',10)
+
+insert into Program values (8,'Administracion de Empresas',10)
+
+insert into Program values (9,'Ingenieria Mecanica',10)
+
+insert into Program values (10,'Ingenieria Biomedica',10)
+
+/*Insertar usuarios*/
+
+insert into User values(1032483872,'Hernan Felipe','Losada Calderon','hernan.losada@gmail.com','2697490','+573158207964',4,2018,'1996-07-29',2)
+
+insert into User values(2105409,'Pipe','Losada','hernan.losada@mail.escuelaing.edu.co','2697490','+17868182661',4,2018,'1996-07-29',2)
+
+/* Insertar Request */
+
+insert into Request values(1032483872,1,'2016-10-9','Probando el envio de correos desaprobados','R')
+
+insert into Request values(1032483872,2,'2016-11-9','Probando el envio de correos desaprobados','A')
---
+++ src/test/java/edu/eci/pdsw/aeci/tests/UsuarioTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2095498 JohanRamirezC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.eci.pdsw.aeci.tests;
+
+
+import java.sql.Connection;
+import java.util.*;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author FelipeLosada
+ * 
+ * 
+ * clases de equivalencia
+ * 
+ * clase1: Enviar Correo Aprobado
+ * falla:No
+ * nombre:EnvioAprobado
+ * 
+ * clase2: Envia Correo no aprobado
+ * falla: No
+ * nombre: EnvioSinAprobar
+ * 
+ */
+public class UsuarioTest {
+    
+     /**
+     * Obtiene una conexion a la base de datos de prueba
+     * @return
+     * @throws SQLException 
+     */
+    private Connection getConnection() throws SQLException{
+        return DriverManager.getConnection("jdbc:h2:file:./target/db/testdb;MODE=MYSQL", "anonymous", "");        
+    }
+    
+    
+    @Test
+    public void EnvioAprobado() {
+       
+    }
+    
+    @Test
+    public void EnvioSinAprobado() {
+       
+    }
+   
+    
+    
+}
